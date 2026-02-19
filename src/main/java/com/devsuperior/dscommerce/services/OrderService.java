e795a3776b82a7af3a22b3e513e75a470ab9df18
Victor Brandi <victorterrenguibrand@gmail.com>
2026-02-19 11:53:38 -0300

    Validate self or admin

--- src/main/java/com/devsuperior/dscommerce/services/OrderService.java
+++ src/main/java/com/devsuperior/dscommerce/services/OrderService.java
@@ -30,10 +30,15 @@ public class OrderService {
     @Autowired
     OrderItemRepository orderItemRepository;
 
+    @Autowired
+    private AuthService authService;
+
     @Transactional(readOnly = true)
     public OrderDTO findById(Long id) {
-        return new OrderDTO(repository.findById(id).
-                orElseThrow( () -> new ResourceNotFoundException( "Recurso não encontrado" ) ));
+        Order order = repository.findById(id).
+                orElseThrow( () -> new ResourceNotFoundException( "Recurso não encontrado" ));
+        authService.validateSelfOrAdmin(order.getClient().getId());
+        return new OrderDTO(order);
     }
 
     @Transactional
